8014ef1b031bebde696c28b2ff83c6776e01a37b
kevin kikubi <kevinkikubi@gmail.com>
2026-07-18 16:44:58 +0100

    core debug 2

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -292,7 +292,18 @@ CakeSession::write('Localisation', 'CG');
       $server_name = env('SERVER_NAME');
       // On enleve les www. pour eviter les blems des fausses redirections
       if (substr($server_name, 0, 4) == 'www.') $server_name = substr_replace($server_name, '', 0, 4);
-      $this->redirect('https://www.' . $server_name . $this->here);
+      
+      // ANCIEN CODE QUI CASSE TOUT SUR L'IP :
+      // $this->redirect('https://www.' . $server_name . $this->here);
+      
+      // NOUVEAU CODE : Si c'est une IP, on ne force pas le HTTPS/WWW pour le test
+      if (filter_var($server_name, FILTER_VALIDATE_IP)) {
+          // On reste en HTTP normal sur l'IP
+          return; 
+      } else {
+          // Comportement normal si c'est un vrai nom de domaine plus tard
+          $this->redirect('https://www.' . $server_name . $this->here);
+      }
     }else{
       $this->Flash->notif(sprintf(__d('translate',  "Erreur, veuillez remplir les champs correctement (%s)"), $type), ['params' => 'echec']);
     }
